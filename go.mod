03a779471f25a7b218b6010fdfa2870072e3817a
Mayorov Dmitriy <dmitriychizh@yandex.ru>
2026-01-08 21:31:27 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pzActions
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+ // Функция возвращает максимальное значение из двух чисел
+func MaxInt(a, b int) int {
+    if a >= b {
+        return a
+    }
+
+    return b
+}
