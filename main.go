108988400134fb391dfb27100f0684f468df634d
Mayorov Dmitriy <dmitriychizh@yandex.ru>
2026-01-08 21:36:27 +0300

    new main code

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-func main() {
+import (
+    "fmt"
+    "sync"
+)
 
+func main() {
+    var wg sync.WaitGroup
+    wg.Add(5)
+
+    for i := 0; i < 5; i++ {
+        go func() {
+            fmt.Println(i)
+            wg.Done()
+        }()
+    }
+
+    wg.Wait()
 }
- // Функция возвращает максимальное значение из двух чисел
+
 func MaxInt(a, b int) int {
     if a >= b {
         return a
